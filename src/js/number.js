df68019d8cfe637534b224efd0dd43d915599004
Anton_Kuzmitsky <tat7106@gmail.com>
2020-10-12 01:24:43 +0600

    create all scripts

--- src/js/number.js
+++ src/js/number.js
@@ -1,11 +1,20 @@
+import {priceValue, planValue, planPrice} from "./plan";
+
 const inputNumber = document.querySelector('input[id="license-count"]');
 
 const handleInputNumberChange = (evt) => {
   const target = evt.target;
   const value = target.value;
+  let totalPrice;
 
   if (+value < 0) {
     target.value = 0;
+    priceValue.textContent = ` $0`;
+  }
+
+  if (planValue && +value >= 0) {
+    totalPrice = +planPrice.textContent * value;
+    priceValue.textContent = ` $${totalPrice}`;
   }
 };
 
--- src/js/plan.js
+++ src/js/plan.js
@@ -1,20 +1,25 @@
 import {inputNumber} from "./number";
 
 const radioButtonsWrapper = document.querySelector('.license__radio-buttons-wrapper');
+const priceValue = document.querySelector('.license__price span');
+const planChoice = document.querySelector('.license__plan-choice');
 let planValue;
+let planPrice;
 
 const handleRadioButtonChange = (evt) => {
   const target = evt.target;
-  
+  planPrice = target.closest('div').querySelector('.license__plan-description span');
+  let totalPrice;
+
   if (!target.closest('input')) return false;
 
   planValue = target.dataset.value;
-  inputNumber.value
+  totalPrice = +planPrice.textContent * inputNumber.value;
 
-  console.log(planValue)
-
+  priceValue.textContent = ` $${totalPrice}`;
+  planChoice.textContent = `Selected plan: #${planValue}`;
 };
 
-// Selected plan: #3
-
 radioButtonsWrapper.addEventListener('change', handleRadioButtonChange);
+
+export {priceValue, planValue, planPrice};
